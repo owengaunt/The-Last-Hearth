52b02f78e2b8d18496f7a9cedbbbe4822209e814
owengaunt <71355571+owengaunt@users.noreply.github.com>
2020-10-12 19:27:30 -0500

    Raised Tree and Character

---
+++ Krum/Assets/Scripts/Inventory.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    private bool inventoryEnabled;
+    public GameObject inventory;
+
+    private int allSlots;
+    private int enabledSlots;
+    private GameObject[] slot;
+
+    public GameObject slotHolder;
+
+    void Start()
+    {
+        allSlots = 39;
+        slot = new GameObject[allSlots];
+       
+            for (int i = 0; i < allSlots; i++)
+        {
+            slot[i] = slotHolder.transform.GetChild(i).gameObject;
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.I))
+            inventoryEnabled = !inventoryEnabled;
+
+        if (inventoryEnabled == true)
+        {
+            inventory.SetActive(true);
+        }
+        else
+        {
+            inventory.SetActive(false);
+        }
+            
+        
+    }
+}
