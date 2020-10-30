d61eb9366bafb308069fe48dfae2cc3004608919
owengaunt <71355571+owengaunt@users.noreply.github.com>
2020-10-30 15:07:02 -0500

    Started new Inventory

---
+++ Krum/Assets/Scripts/Player/Inventory/Item Scripts/EquipmentObject.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(fileName = "New Default Equipment", menuName = "Inventory System/Items/Equipment")]
+
+public class EquipmentObject : ItemObject
+{
+    public float atkBonus;
+    public float dfsBonus;
+
+    public void Awake()
+    {
+        type = ItemType.Equipment;
+    }
+}
---
+++ Krum/Assets/Scripts/Player/Inventory/Item Scripts/FoodObject.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(fileName = "New Food Object", menuName = "Inventory System/Items/Food")]
+
+public class FoodObject : ItemObject
+{
+    public int restorehungervalue;
+
+    public void Awake()
+    {
+        type = ItemType.Food;
+    }
+
+}
