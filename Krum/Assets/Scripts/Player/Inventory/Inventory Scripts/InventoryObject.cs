a8c6c5c66b3902a067c9d50be010115c3d77f652
owengaunt <71355571+owengaunt@users.noreply.github.com>
2020-11-02 08:21:45 -0600

    Save Function Started

---
+++ Krum/Assets/Scripts/Player/Inventory/Inventory Scripts/InventoryObject.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Runtime.Hosting;
+using System.IO;
+
+[CreateAssetMenu(fileName = "New Inventory", menuName = "Inventory System/Inventory")]
+public class InventoryObject : ScriptableObject, ISerializationCallbackReceiver
+{
+    public string savePath;
+    public ItemDatabaseObject database;
+    public List<InventorySlot> Container = new List<InventorySlot>();
+    public void AddItem(ItemObject _item, int _amount)
+    {
+        for (int i = 0; i < Container.Count; i++)
+        {
+            if(Container[i].item == _item)
+            {
+                Container[i].AddAmount(_amount);
+                return;
+            }
+           
+        }
+            Container.Add(new InventorySlot(database.GetId[_item], _item, _amount));
+        }
+
+    public Save()
+    {
+        string saveData = JsonUtility.ToJson(this, true);
+        BinaryFormatter bf = new BinaryFormatter();
+        FileStream file = file.Create(string.Concact(Application.persistentDataPath, savePath));
+        bf.Serialize(file, saveData);
+        file.Close();
+    }
+    
+    
+    public Load()
+    {
+        if(File.Exists(string.Concat(Application.persistentDatapath, savePath)))
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            FileStream file = File.Open(string.Concat(Application.persistentDatapath, savePath), FileMode.Open);
+            JsonUtility.FromJsonOverwrite(bf.Deserialize(file). ToString(), this);
+            file.Close();
+        }
+    }
+
+    public void OnAfterDeserialize()
+    {
+        for (int i = 0; i < Container.Count; i++)       
+            Container[i].item = database.GetItem[Container[i].ID];     
+    }
+
+    public void OnBeforeSerialize()
+    {
+    }
+}
+
+[System.Serializable]
+public class InventorySlot
+{
+    public int ID;
+    public ItemObject item;
+    public int amount;
+    public InventorySlot(int _id, ItemObject _item, int _amount)
+    {
+        ID = _id;
+        item = _item;
+        amount = _amount;
+    }
+
+    public void AddAmount(int value)
+    {
+        amount += value;
+    }
+
+}
---
+++ Krum/Assets/Scripts/Player/Inventory/Inventory Scripts/PlayerINV.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerINV : MonoBehaviour
+{
+
+    public InventoryObject inventory; 
+
+    public void OnTriggerEnter(Collider other)
+    {
+        var item = other.GetComponent<Item>();
+        if (item)
+        {
+            inventory.AddItem(item.item, 1);
+            Destroy(other.gameObject);
+        }
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Y))
+        {
+            inventory.Save();
+        }
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            inventory.Load();
+        }
+    }
+
+
+
+    private void OnApplicationQuit()
+    {
+        inventory.Container.Clear();
+    }
+
+}
