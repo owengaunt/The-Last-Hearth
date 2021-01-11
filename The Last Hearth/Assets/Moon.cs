0ae087f9bac5bce50487af9e3b57e97bca5b6f10
owengaunt <71355571+owengaunt@users.noreply.github.com>
2021-01-11 10:34:38 -0600

    Moon Sun Colour and Shadow Fix
    There is no Snow

---
+++ The Last Hearth/Assets/Moon.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoonObj : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(Vector3.zero);
+    }
+}
