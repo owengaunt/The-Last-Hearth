208f19a48d093f98bae340ea1912683d2bc279d1
owengaunt <71355571+owengaunt@users.noreply.github.com>
2021-01-12 11:58:12 -0600

    Revert "Moon Sun Colour and Shadow Fix"
    This reverts commit 0ae087f9bac5bce50487af9e3b57e97bca5b6f10.

--- The Last Hearth/Assets/Scripts/Inventory/Inventory Scripts/InventoryObject.cs
+++ The Last Hearth/Assets/Scripts/Inventory/Inventory Scripts/InventoryObject.cs
@@ -102,6 +102,12 @@ public class InventoryObject : ScriptableObject
     [ContextMenu("Save")]
     public void Save()
     {
+        //    //string saveData = JsonUtility.ToJson(this, true);
+        //    //BinaryFormatter bf = new BinaryFormatter();
+        //    //FileStream file = File.Create(string.Concat(Application.persistentDataPath, savePath));
+        //    //bf.Serialize(file, saveData);
+        //    //file.Close();
+
         IFormatter formatter = new BinaryFormatter();
         Stream stream = new FileStream(string.Concat(Application.persistentDataPath, savePath), FileMode.Create, FileAccess.Write);
         formatter.Serialize(stream, Container);
@@ -111,6 +117,13 @@ public class InventoryObject : ScriptableObject
     [ContextMenu("Load")]
     public void Load()
     {
+        //    if(File.Exists(string.Concat(Application.persistentDataPath, savePath)))
+        //    {
+        //        //BinaryFormatter bf = new BinaryFormatter();
+        //        //FileStream file = File.Open(string.Concat(Application.persistentDataPath, savePath), FileMode.Open);
+        //        //JsonUtility.FromJsonOverwrite(bf.Deserialize(file).ToString(), this);
+        //        //file.Close();
+
         IFormatter formatter = new BinaryFormatter();
         Stream stream = new FileStream(string.Concat(Application.persistentDataPath, savePath), FileMode.Open, FileAccess.Read);
         Inventory newContainer = (Inventory)formatter.Deserialize(stream);
